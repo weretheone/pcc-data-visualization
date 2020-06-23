90e5ff603e738b002daf90330b8dc01d4bca2a70
weretheone <verestamas86@gmail.com>
2020-06-23 20:04:42 +0200

    Finished updates

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -28,33 +28,3 @@ for repo_dict in repo_dicts:
     label = f"{owner}<br />{description}"
     labels.append(label)
 
-# Make visualization.
-data = [{
-   'type': 'bar',
-   'x': repo_links,
-   'y': stars,
-   'hovertext': labels,
-   'marker': {
-        'color': 'rgb(60, 100, 150)',
-        'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
-    },
-    'opacity': 0.6,
-}]
-my_layout = {
-   'title': 'Most-Starred Python Projects on GitHub',
-   'titlefont': {'size': 28},
-   'xaxis': {
-        'title': 'Repository',
-        'titlefont': {'size': 24},
-        'tickfont': {'size': 14},
-    },
-   'xaxis': {'title': 'Repository'},
-   'yaxis': {
-        'title': 'Stars',
-        'titlefont': {'size': 24},
-        'tickfont': {'size': 14},
-    },
-}
-
-fig = {'data': data, 'layout': my_layout}
-offline.plot(fig, filename='python_repos.html')
